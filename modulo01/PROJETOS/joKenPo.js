499d2dee498da7a38ffe2c10dd05b66d13a6177c
thomasmoura07 <99275405+thomasmoura07@users.noreply.github.com>
2022-03-18 16:24:37 -0300

    Adição de Validação de dados

--- modulo01/PROJETOS/joKenPo.js
+++ modulo01/PROJETOS/joKenPo.js
@@ -1,86 +1,120 @@
-const prompt = require('prompt-sync')();
+const prompt = require("prompt-sync")();
 console.clear();
 
 // O Jokenpô é o popular jogo do "pedra, papel e tesoura". A premissa é simples, você tem 3 elementos para escolher, e cada um deles vence de um, e perde para o outro.
 
-const gameOption = ['papel','pedra','tesoura']
-let player,bot,rodadasI,scoreP = 0,scoreB = 0,continua
+const gameOption = ["papel", "pedra", "tesoura"];
+let continua;
 
+//RESPONSÁVEL POR REINICALIZAR O JOGO CASO SEJA REQUISITADO.
+do {
+  let player,
+    bot,
+    rodadasI,
+    scoreP = 0,
+    scoreB = 0,
+    scoreD = 0;
 
-do { 
-
-  do{ 
-rodadasI = prompt('Quantas rodadas quer jogar?(min: 1; max: 10)')
-  console.clear();
-  }while(isNaN(rodadasI) || rodadasI == 0 || rodadasI > 10)
-
-  
-   for(let i = 1; i <= rodadasI; i++) {
-    console.log(`Rodada ${i} de ${rodadasI} rodada(s)`)
-    player = prompt('Esolha: | Papel | Pedra | Tesoura |------>').toLowerCase();
-
-    while(player != 'papel' && player != 'pedra' && player != 'tesoura'){ 
+  do {
+    rodadasI = prompt("Quantas rodadas quer jogar?(min: 1; max: 10)");
     console.clear();
-    console.log(`Rodada ${i} de ${rodadasI} rodada(s)`)
-    player = prompt('Esolha: | Papel | Pedra | Tesoura |------>').toLowerCase();
+  } while (isNaN(rodadasI) || rodadasI == 0 || rodadasI > 10);
 
-    console.clear();
-    
-  } 
-    console.clear();
-    
-    switch (player) {
-      case 'papel': player = 0;
-      break;
-      case 'pedra': player = 1;
-      break;
-      case 'tesoura': player =2;
-      break;
+  // RODADAS: VAI REPETIR O JOGO DE ACORDO COM A VARIÁVEL 'rodadasI' DEFINIDA PELO USURÁRIO.
+  for (let i = 1; i <= rodadasI; i++) {
+    console.log(`Rodada ${i} de ${rodadasI} rodada(s)`);
+    player = prompt("Esolha: | Papel | Pedra | Tesoura |------>").toLowerCase();
+
+    while (player != "papel" && player != "pedra" && player != "tesoura") {
+      console.clear();
+      console.log(`Rodada ${i} de ${rodadasI} rodada(s)`);
+      player = prompt(
+        "Esolha: | Papel | Pedra | Tesoura |------>"
+      ).toLowerCase();
+
+      console.clear();
     }
-  
-    bot = Math.floor(Math.random()*3);
+    console.clear();
+    // CONVERTER A STRING PARA NÚMEROS PARA SER USADO COM A ARRAY 'gameOption'
+    switch (player) {
+      case "papel":
+        player = 0;
+        break;
+      case "pedra":
+        player = 1;
+        break;
+      case "tesoura":
+        player = 2;
+        break;
+    }
 
-    console.log(`Rodada ${i} de ${rodadasI} rodada(s)`)
-    
-    if ((gameOption[player] == 'papel' && gameOption[bot] == 'pedra') || (gameOption[player] == 'pedra' && gameOption[bot] == 'tesoura') || (gameOption[player] == 'tesoura' && gameOption[bot] == 'papel')) {
+    bot = Math.floor(Math.random() * 3);
+
+    console.log(`Rodada ${i} de ${rodadasI} rodada(s)`);
+
+    if (
+      (gameOption[player] == "papel" && gameOption[bot] == "pedra") ||
+      (gameOption[player] == "pedra" && gameOption[bot] == "tesoura") ||
+      (gameOption[player] == "tesoura" && gameOption[bot] == "papel")
+    ) {
       scoreP++;
       console.log();
-      console.log(`${scoreP} PLAYER | ${scoreB} CPU`)
+      console.log(`${scoreP} PLAYER | ${scoreB} CPU`);
       console.log();
-      console.log(`player: ${gameOption[player]}---X--${gameOption[bot]} (CPU)`);
+      console.log(
+        `player: ${gameOption[player]}---X--${gameOption[bot]} (CPU)`
+      );
       console.log(`Você ganhou`);
-     
-    
-    } else if ((gameOption[player] == 'papel' && gameOption[bot] == 'tesoura') || (gameOption[player] == "tesoura" && gameOption[bot] == 'pedra') || (gameOption[player] == 'pedra' && gameOption[bot] == 'papel')) {
+    } else if (
+      (gameOption[player] == "papel" && gameOption[bot] == "tesoura") ||
+      (gameOption[player] == "tesoura" && gameOption[bot] == "pedra") ||
+      (gameOption[player] == "pedra" && gameOption[bot] == "papel")
+    ) {
       scoreB++;
       console.log();
-      console.log(`${scoreP} PLAYER | ${scoreB} CPU`)
+      console.log(`${scoreP} PLAYER | ${scoreB} CPU`);
       console.log();
-      console.log(`player: ${gameOption[player]}---X--${gameOption[bot]} (CPU)`);
+      console.log(
+        `player: ${gameOption[player]}---X--${gameOption[bot]} (CPU)`
+      );
       console.log(`Você perdeu`);
-      
-
-    } else if ((gameOption[player] == 'papel' && gameOption[bot] == 'papel') || (gameOption[player] == 'pedra' && gameOption[bot] == 'pedra') || (gameOption[player] == 'tesoura' && gameOption[bot] == 'tesoura')){
+    } else if (
+      (gameOption[player] == "papel" && gameOption[bot] == "papel") ||
+      (gameOption[player] == "pedra" && gameOption[bot] == "pedra") ||
+      (gameOption[player] == "tesoura" && gameOption[bot] == "tesoura")
+    ) {
       console.log();
-      console.log(`${scoreP} PLAYER | ${scoreB} CPU`)
+      console.log(`${scoreP} PLAYER | ${scoreB} CPU`);
       console.log();
-      console.log(`player: ${gameOption[player]}---X--${gameOption[bot]} (CPU)`);
-      console.log('Empate');
-
+      console.log(
+        `player: ${gameOption[player]}---X--${gameOption[bot]} (CPU)`
+      );
+      console.log("Empate");
+      scoreD++;
     }
     continua = prompt();
     console.clear();
   }
   console.log();
   if (scoreP > scoreB) {
-    console.log("Você venceu a máquina!")
+    console.log("Você venceu a máquina!");
   } else if (scoreP < scoreB) {
-    console.log('Você perdeu para a máquina!')
-  } else {console.log('Jogo Empatado!')}
+    console.log("Você perdeu para a máquina!");
+  } else {
+    console.log("Jogo Empatado!");
+  }
+  console.log();
+  console.log(`Vitórias: ${scoreP}\nDerrotas: ${scoreB}\nEmpates: ${scoreD}`);
 
   console.log();
-
-  continua = prompt('Deseja jogar novamente (s/n):').toLowerCase();
-  console.clear();
-
-}while(continua == 'sim' || continua == 's')
+  do {
+    //VALIDAÇÃO DE ENTRADA DE DADOS
+    continua = prompt("Deseja jogar novamente (s/n):").toLowerCase();
+    console.clear();
+  } while (
+    continua != "s" &&
+    continua != "sim" &&
+    continua != "n" &&
+    continua != "nao"
+  );
+} while (continua == "sim" || continua == "s");
